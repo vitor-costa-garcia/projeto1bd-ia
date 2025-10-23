c98dae0b549534207d6bfa3dc50dbff9e3da3112
sschettinii <andreschettini2006@gmail.com>
2025-10-23 18:54:28 -0300

    analysis: main.sql: comparei o modelo relacional com a main.sql reportando/ajustando insconsistencias entre os modelos

--- sql/main.sql
+++ sql/main.sql
@@ -1,12 +1,14 @@
+USUARIO_ESP = {PK FK ID_USUARIO, PK ESPECIALIZACAO}\
 -- CREATE DATABASE sistema_comp_ia;
 
+USUARIO = {PK ID, NOME, EMAIL, SENHA, DATANASCIMENTO, NUMERO, RUA, CIDADE, ESTADO, PAIS}
 CREATE TABLE usuario(
     id SERIAL NOT NULL,
     nome VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL,
     senha CHAR(128) NOT NULL,
     datanascimento DATE NOT NULL,
-    numero INT,
+    numero VARCHAR(25),
     rua VARCHAR(50),
     cidade VARCHAR(50),
     estado VARCHAR(50),
@@ -14,6 +16,7 @@ CREATE TABLE usuario(
     CONSTRAINT "PK_ID_USUARIO" PRIMARY KEY (id)
 );
 
+USUARIO_TIPO = {PK FK ID_USUARIO, PK TIPO}
 CREATE TABLE usuario_tipo(
     id_usuario INT NOT NULL,
     tipo INT NOT NULL CHECK(tipo = 0 OR tipo = 1),
@@ -21,34 +24,39 @@ CREATE TABLE usuario_tipo(
     CONSTRAINT "FK_ID_USUARIO_TIPO" FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
 
+ORGANIZADOR = {PK FK ID_USUARIO, CPF}
 CREATE TABLE organizador(
     id_usuario INT NOT NULL,
     cpf VARCHAR(14) NOT NULL,
     CONSTRAINT "PK_ID_USUARIO_ORGANIZADOR" PRIMARY KEY (id_usuario),
-    CONSTRAINT "UNIQUE_CPF_ORGANIZADOR" UNIQUE (cpf)
+    CONSTRAINT "UNIQUE_CPF_ORGANIZADOR" UNIQUE (cpf),
+    CONSTRAINT "FK_ID_USUARIO_ORGANIZADOR" FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
 
+PATROCINADOR = {PK CNPJ, NOME_FANTASIA, ENDERECO, TELEFONE}
 CREATE TABLE patrocinador(
     cnpj VARCHAR(18) NOT NULL,
     nome_fantasia VARCHAR(30) NOT NULL,
     endereco VARCHAR(100) NOT NULL,
-    telefone VARCHAR(14) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
     CONSTRAINT "PK_PATROCINADOR" PRIMARY KEY (cnpj)
 );
 
+COMPETICAO_PRED = {PK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, FLG_OFICIAL, TITULO, DESCRICAO, DIFICULDADE, DATA_CRIAÇÃO, DATA_INICIO, DATA_FIM, MÉTRICA_DESEMPENHO, DATASET_TT, DATASET_SUBMISSAO, DATASET_GABARITO, FK CNPJ_PATROCINADOR, PREMIACAO}
 CREATE TABLE competicao_pred(
     id_competicao SERIAL NOT NULL,
     id_org_competicao INT NOT NULL,
     flg_oficial INT NOT NULL CHECK(flg_oficial BETWEEN 0 AND 1),
-    dataset_tt VARCHAR(200) NOT NULL, -- Caminho do arquivo
-    dataset_submissao VARCHAR(200) NOT NULL, --
-    dataset_gabarito VARCHAR(200) NOT NULL, --
+    dataset_tt VARCHAR(200) NOT NULL,
+    dataset_submissao VARCHAR(200) NOT NULL,
+    dataset_gabarito VARCHAR(200) NOT NULL,
     cnpj_patrocinador VARCHAR(18) CHECK((flg_oficial = 1 AND cnpj_patrocinador IS NOT NULL) OR (flg_oficial = 0 AND cnpj_patrocinador IS NULL)),
     premiacao MONEY CHECK((flg_oficial = 1 AND cnpj_patrocinador IS NOT NULL) OR (flg_oficial = 0 AND cnpj_patrocinador IS NULL)),
     CONSTRAINT "PK_COMPETICAO_PRED" PRIMARY KEY (id_competicao, id_org_competicao),
     CONSTRAINT "FK_ID_PATROCINADOR_COMP_PRED" FOREIGN KEY (cnpj_patrocinador) REFERENCES patrocinador(cnpj)
 );
 
+COMPETICAO_REGRAS_PRED = {PK FK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, PK N_ORDEM, REGRA}
 CREATE TABLE competicao_regras_pred(
     id_competicao INT NOT NULL,
     id_org_competicao INT NOT NULL,
@@ -58,6 +66,7 @@ CREATE TABLE competicao_regras_pred(
     CONSTRAINT "FK_REGRAS_COMPETICAO_PRED" FOREIGN KEY (id_competicao, id_org_competicao) REFERENCES competicao_pred(id_competicao,  id_org_competicao)
 );
 
+EQUIPE_PRED = {PK ID, PK FK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, NOME}\
 CREATE TABLE equipe_pred(
     id SERIAL NOT NULL,
     id_competicao INT NOT NULL,
@@ -67,6 +76,7 @@ CREATE TABLE equipe_pred(
     CONSTRAINT "FK_EQUIPE_COMP_PRED" FOREIGN KEY (id_competicao, id_org_competicao) REFERENCES competicao_pred(id_competicao, id_org_competicao)
 );
 
+AGGR_COMPOE_EQUIPE_PRED = {PK FK ID_EQUIPE, PK FK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, PK FK ID_COMPETIDOR, PK DATA_HORA_INICIO, DATA_HORA_FIM}\
 CREATE TABLE composicao_equipe_pred(
     id_equipe INT NOT NULL,
     id_competicao INT NOT NULL,
@@ -79,6 +89,7 @@ CREATE TABLE composicao_equipe_pred(
     CONSTRAINT "FK_PART_EQUIPE_COMPETIDOR_COMP_PRED" FOREIGN KEY (id_competidor) REFERENCES usuario(id) 
 );
 
+AGGR_PREMIO_COMPETIDOR_PRED = {PK FK ID_COMPETIDO, PK FK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, PK DATA_RECEBIMENTO, PK TIPO, CLASSIFICACAO, VALOR}
 CREATE TABLE premios_competidor_pred(
     id_competidor INT NOT NULL,
     id_competicao INT NOT NULL,
@@ -92,6 +103,7 @@ CREATE TABLE premios_competidor_pred(
     CONSTRAINT "FK_COMPETIDOR_COMP_PRED" FOREIGN KEY (id_competidor) REFERENCES usuario(id)
 );
 
+AGGR_SUBMISSAO_EQUIPE_PRED = {PK FK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, PK FK ID_EQUIPE, PK DATA_HORA_ENVIO, ARQ_SUBMISSAO, SCORE}
 CREATE TABLE submissao_equipe_pred(
     id_competicao INT NOT NULL,
     id_org_competicao INT NOT NULL,
@@ -103,6 +115,7 @@ CREATE TABLE submissao_equipe_pred(
     CONSTRAINT "FK_SUBMISSAO_EQUIPE_PRED" FOREIGN KEY (id_competicao, id_org_competicao, id_equipe) REFERENCES equipe_pred(id_competicao, id_org_competicao, id)
 )
 
+COMPETICAO_SIMUL = {PK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, FLG_OFICIAL, TITULO, DESCRICAO, DIFICULDADE, DATA_CRIAÇÃO, DATA_INICIO, DATA_FIM, MÉTRICA_DESEMPENHO, AMBIENTE, FK CNPJ_PATROCINADOR, PREMIACAO}
 CREATE TABLE competicao_simul(
     id_competicao SERIAL NOT NULL,
     id_org_competicao INT NOT NULL,
@@ -114,6 +127,7 @@ CREATE TABLE competicao_simul(
     CONSTRAINT "FK_ID_PATROCINADOR_COMP_SIMUL" FOREIGN KEY (cnpj_patrocinador) REFERENCES patrocinador(cnpj)
 );
 
+COMPETICAO_REGRAS_SIMUL = {PK FK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, PK N_ORDEM, REGRA}\
 CREATE TABLE competicao_regras_simul(
     id_competicao INT NOT NULL,
     id_org_competicao INT NOT NULL,
@@ -123,6 +137,7 @@ CREATE TABLE competicao_regras_simul(
     CONSTRAINT "FK_COMPETICAO_SIMUL" FOREIGN KEY (id_competicao, id_org_competicao) REFERENCES competicao_simul(id_competicao, id_org_competicao)
 );
 
+EQUIPE_SIMUL = {PK ID, PK FK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, NOME}
 CREATE TABLE equipe_simul(
     id SERIAL NOT NULL,
     id_competicao INT NOT NULL,
@@ -132,6 +147,7 @@ CREATE TABLE equipe_simul(
     CONSTRAINT "FK_EQUIPE_COMP_SIMUL" FOREIGN KEY (id_competicao, id_org_competicao) REFERENCES competicao_simul(id_competicao, id_org_competicao)
 );
 
+AGGR_COMPOE_EQUIPE_SIMUL = {PK FK ID_EQUIPE, PK FK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, PK FK ID_COMPETIDOR, PK DATA_HORA_INICIO, DATA_HORA_FIM}\
 CREATE TABLE composicao_equipe_simul(
     id_equipe INT NOT NULL,
     id_competicao INT NOT NULL,
@@ -144,6 +160,7 @@ CREATE TABLE composicao_equipe_simul(
     CONSTRAINT "FK_PART_EQUIPE_COMPETIDOR_COMP_SIMUL" FOREIGN KEY (id_competidor) REFERENCES usuario(id) 
 );
 
+AGGR_PREMIO_COMPETIDOR_SIMUL = {PK FK ID_COMPETIDOR,PK FK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, PK DATA_RECEBIMENTO, PK TIPO, CLASSIFICACAO, VALOR}
 CREATE TABLE premios_competidor_simul(
     id_competidor INT NOT NULL,
     id_competicao INT NOT NULL,
@@ -157,6 +174,7 @@ CREATE TABLE premios_competidor_simul(
     CONSTRAINT "FK_COMPETIDOR_COMP_SIMUL" FOREIGN KEY (id_competidor) REFERENCES usuario(id)
 );
 
+AGGR_SUBMISSAO_EQUIPE_SIMUL = {PK FK ID_COMPETICAO, PK FK ID_ORG_COMPETICAO, PK FK ID_EQUIPE, PK DATA_HORA_ENVIO, ARQ_SUBMISSAO, SCORE}
 CREATE TABLE submissao_equipe_simul(
     id_competicao INT NOT NULL,
     id_org_competicao INT NOT NULL,
