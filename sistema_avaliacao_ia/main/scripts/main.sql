0fb642a62560e27b25ac42b04745d6a6959b3fee
sschettinii <andreschettini2006@gmail.com>
2025-11-09 17:26:40 -0300

    add: pagina de login e registro + funcionalidade de registrar e logar usuarios

--- sistema_avaliacao_ia/main/scripts/main.sql
+++ sistema_avaliacao_ia/main/scripts/main.sql
@@ -2,7 +2,7 @@ CREATE TABLE usuario(
     id SERIAL NOT NULL,
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    senha CHAR(128) NOT NULL,
+    senha VARCHAR(255) NOT NULL,
     datanascimento DATE NOT NULL,
     numero VARCHAR(25),
     rua VARCHAR(50),
