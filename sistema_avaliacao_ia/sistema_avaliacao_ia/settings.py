9d6cc702bdd9e66c0d6387e6876d7b95d01ef7ec
sschettinii <andreschettini2006@gmail.com>
2025-11-25 22:43:10 -0300

    feat: implement session timeout feature and update session management settings

--- sistema_avaliacao_ia/sistema_avaliacao_ia/settings.py
+++ sistema_avaliacao_ia/sistema_avaliacao_ia/settings.py
@@ -113,4 +113,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
+SESSION_COOKIE_AGE = 900
+SESSION_SAVE_EVERY_REQUEST = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
